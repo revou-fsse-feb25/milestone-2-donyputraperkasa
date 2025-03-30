4b68d497892b1b26254ad554bdda7b89b515d9dc
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2025-03-30 14:13:16 +0700

    feat : add javascript

--- beforelogin.js
+++ beforelogin.js
@@ -3,9 +3,14 @@ const pass = document.getElementById("pass");
 const login = document.getElementById("login");
 const welcome = document.querySelector("h2");
 
-function pesan1(){
+function pesan(){
     alert("selamat datang " + nama.value);
 }
-function pesan2(){
-    welcome.textContent = "selamat datang " + nama.value;
-}
+
+let button = document.getElementById("buttonLogin");
+
+button.style.backgroundColor = "blue-500";
+button.style.color = "blue-500";
+button.addEventListener("click", function(){
+    button.style.backgroundColor = "blue-700";
+})
