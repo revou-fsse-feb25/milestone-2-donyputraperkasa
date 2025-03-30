f031124ac4ee0f7f869159935a8917ef191f40c9
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2025-03-30 13:43:40 +0700

    feat : add all content - try 2

---
+++ calculatorMath/calculatorMath.js
@@ -0,0 +1,7 @@
+function openSidebar() {
+    document.getElementById("sidebar").classList.remove("ml-[-100%]");
+   }
+
+   function closeSidebar() {
+    document.getElementById("sidebar").classList.add("ml-[-100%]");
+   }
