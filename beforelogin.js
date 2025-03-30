a8cbf3e161b74362f9136499f309cc87649408f9
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2025-03-30 14:23:21 +0700

    feat : add content home + color button login 3

--- beforelogin.js
+++ beforelogin.js
@@ -6,11 +6,3 @@ const welcome = document.querySelector("h2");
 function pesan(){
     alert("selamat datang " + nama.value);
 }
-
-let button = document.getElementById("buttonLogin");
-
-button.style.backgroundColor = "blue-500";
-button.style.color = "blue-500";
-button.addEventListener("click", function(){
-    button.style.backgroundColor = "blue-700";
-})
